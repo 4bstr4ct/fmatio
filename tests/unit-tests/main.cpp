9032483d54749ce637f2fc16e1a522860fa771bf
Abstract <57039383+4bstr4ct@users.noreply.github.com>
2022-03-13 21:38:51 +0200

    figured char pointer assignment error

--- tests/unit-tests/main.cpp
+++ tests/unit-tests/main.cpp
@@ -17,8 +17,22 @@ public:
 	float finalGrade;
 };
 
+const char* test()
+{
+	char* array = new char[11];
+	for (uint32_t i = 0 ; i < 10; i++)
+		array[i] = 'e';
+	array[10] = '\0';
+	return array;
+}
+
 int main(int argc, char** argv)
 {
+	const char* t = test();
+	std::cout << t << std::endl;
+	delete[] t;
+
+	/*
 	std::cout << fmatio::format<
 		fmatio::BasicArgumentsFormatter<char>,
 			fmatio::BasicString<char>>("Hello, {p:2}!\n", 7.0f).getData();
@@ -28,6 +42,7 @@ int main(int argc, char** argv)
 
 	fmatio::echo<fmatio::ArgumentsFormatter, fmatio::String>(std::cout, "Hello, {p:2}!\n", 7.0f);
 	fmatio::cecho(std::cout, "Hello, {}!\n", "world");
+	*/
 
 	/*
 	{
@@ -56,6 +71,7 @@ int main(int argc, char** argv)
 	*/
 
 	{
+		/*
 		fmatio::cecho(std::cout, "{}\n", 5);
 		fmatio::cecho(std::cout, "{}\n", 7);
 		fmatio::cecho(std::cout, "{}\n", 7u);
@@ -68,15 +84,30 @@ int main(int argc, char** argv)
 		fmatio::cecho(std::cout, "{}\n", "hello again");
 		fmatio::cecho(std::cout, "{}\n", "lalalalalalala!");
 		fmatio::cecho(std::cout, "{}\n", nullptr);
+		*/
+		
+		/*
 		std::cout << fmatio::cformat("{}\n", 8.5f).getData();
+
 		std::string string = fmatio::cformat("{}\n", 8.5f).getData();
 		const char* temp = string.data();
 		std::cout << temp;
+
 		fmatio::BasicString<char> basiString = fmatio::cformat("{}\n", 8.5f);
 		const char* cstring = basiString.getData();
 		std::cout << cstring;
-		const char* cstring2 = fmatio::cformat("{}\n", 8.5f).getData();
+		*/
+		char* cstring2 = nullptr;
+		fmatio::BasicString<char> b = fmatio::cformat("{}\n", 8.5f);
+		cstring2 = b.getData();
 		std::cout << cstring2;
+
+
+		std::cout << str;
+
+		fmatio::BasicStringView<char> view = fmatio::cformat("{}\n", 8.5f).getData();
+		std::cout << view.getData();
+		std::cout << fmatio::cformat("{}\n", 8.5f).getData();
 	}
 
 	return 0;
